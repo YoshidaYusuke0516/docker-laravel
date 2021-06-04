370883dc95c1e7ba185e1b78caf109f2701b0347
Yusuke Yoshida <nt16.k012c1367@gmail.com>
2021-06-04 17:50:59 +0900

    create list

---
+++ backend/resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>List TOP Page</title>
+</head>
+<body>
+  <div>
+    <p>list2</p>
+    <p>list2</p>
+    <p>list3</p>
+  </div>
+</body>
+</html>
